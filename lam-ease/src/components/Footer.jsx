0b353ef3e3a79725f7b725f5b5e1c47cc9558393
shatakd00 <shatakdatta00@gmail.com>
2026-05-21 22:44:33 +0530

    made homepage component files

---
+++ lam-ease/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="footer">
+        <p>&copy; 2024 LamEase. All rights reserved.</p>
+        <div className="social-links">
+            <a href="/">Facebook</a>
+            <a href="/">Twitter</a>
+            <a href="/">LinkedIn</a>
+            <a href="/">Instagram</a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
